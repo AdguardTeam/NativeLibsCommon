b0eb169d17db886c1fc6169b8a0dbf4a42662e27
grumaxxx <m.grupper@adguard.com>
2021-11-30 14:56:14 +0300

    update /sh to export NaviveLibsCommon

--- conan/recipes/export.sh
+++ conan/recipes/export.sh
@@ -3,3 +3,5 @@
 for RECIPE_DIR in $(find . -type d -maxdepth 1 -mindepth 1); do
     conan export "$RECIPE_DIR" AdguardTeam/NativeLibsCommon
 done
+
+conan export ../../common AdguardTeam/NativeLibsCommon
