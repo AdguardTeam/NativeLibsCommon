f6ebe6ab62a7d69e00a251f963b0df997463714c
grumaxxx <m.grupper@adguard.com>
2021-12-01 12:34:06 +0300

    add python version and subprocess

--- scripts/export_conan.py
+++ scripts/export_conan.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
+import subprocess
 
 work_dir = os.path.dirname(os.path.realpath(__file__))
 project_dir = os.path.dirname(work_dir)
@@ -9,6 +10,6 @@ recipes_dir = os.path.join(project_dir, 'conan', 'recipes')
 for folder in os.listdir(recipes_dir):
     path = os.path.join(recipes_dir, folder)
     if os.path.isdir(path):
-        os.system(f"conan export {path} AdguardTeam/NativeLibsCommon")
+        subprocess.run(["conan", "export", path, "AdguardTeam/NativeLibsCommon"])
 
-os.system(f"conan export {project_dir} AdguardTeam/NativeLibsCommon")
+subprocess.run(["conan", "export", project_dir, "AdguardTeam/NativeLibsCommon"])
