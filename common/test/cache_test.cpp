afe90b50ff4f7f90a9d209e03a36135ec5db960e
Nikita Gorskikh <n.gorskikh@adguard.com>
2022-07-19 16:58:31 +0300

    Pull request #47: Port LruTimeoutCache from vpn-client
    Merge in ADGUARD-CORE-LIBS/native-libs-common from feature/lru_timeout_cache to master
    
    Squashed commit of the following:
    
    commit 82f14d5ad843808a5ca7d92e67c25f97e9616417
    Author: Nikita Gorskikh <n.gorskikh@adguard.com>
    Date:   Tue Jul 19 16:32:09 2022 +0300
    
        Port LruTimeoutCache from vpn-client

--- common/test/cache_test.cpp
+++ common/test/cache_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "common/cache.h"
+#include "common/clock.h"
 
 static constexpr size_t CACHE_SIZE = 1000u;
 
@@ -128,4 +129,111 @@ TEST_F(LruCacheTest, UpdateCapacity) {
     for (size_t i = 0; i < CACHE_SIZE / 2; ++i) {
         ASSERT_FALSE(m_cache.get(i)) << i << std::endl;
     }
+}
+
+static constexpr size_t TIMEOUT_MS = 1000u;
+
+TEST(LruTimeoutCache, Timeout) {
+    ag::LruTimeoutCache<int, std::string> cache(CACHE_SIZE, std::chrono::milliseconds(TIMEOUT_MS), false);
+    cache.insert(1, "val");
+    cache.insert(2, "val");
+    cache.insert(3, "val");
+    cache.insert(4, "val", std::chrono::milliseconds(TIMEOUT_MS * 4));
+    cache.insert(5, "val", std::chrono::milliseconds(TIMEOUT_MS));
+
+    ag::SteadyClock::add_time_shift(std::chrono::milliseconds(TIMEOUT_MS * 15 / 10));
+
+    // call get to refresh entry and add new one
+    ASSERT_NE(cache.get(3), nullptr);
+    cache.insert(6, "val");
+
+    ag::SteadyClock::add_time_shift(std::chrono::milliseconds(TIMEOUT_MS * 7 / 10));
+
+    // check that timed out entries are removed after update
+    cache.update();
+    ASSERT_EQ(cache.get(1), nullptr);
+    ASSERT_EQ(cache.get(2), nullptr);
+    ASSERT_NE(cache.get(3), nullptr);
+    ASSERT_NE(cache.get(4), nullptr);
+    ASSERT_EQ(cache.get(5), nullptr);
+    ASSERT_NE(cache.get(6), nullptr);
+}
+
+TEST(LruTimeoutCache, AutoUpdate) {
+    ag::LruTimeoutCache<int, std::string> autoCacheInsert(CACHE_SIZE, std::chrono::milliseconds(TIMEOUT_MS), true);
+    autoCacheInsert.insert(1, "val");
+
+    ag::LruTimeoutCache<int, std::string> autoCacheGet(CACHE_SIZE, std::chrono::milliseconds(TIMEOUT_MS), true);
+    autoCacheGet.insert(1, "val");
+
+    ag::LruTimeoutCache<int, std::string> manCache(CACHE_SIZE, std::chrono::milliseconds(TIMEOUT_MS), false);
+    manCache.insert(1, "val");
+
+    ag::SteadyClock::add_time_shift(std::chrono::milliseconds(TIMEOUT_MS * 2));
+
+    // check that cache performs update only if autoUpdate flag is set and does not otherwise
+    autoCacheInsert.insert(2, "val");
+    manCache.insert(2, "val");
+
+    autoCacheGet.get(2);
+    manCache.get(2);
+
+    ASSERT_EQ(autoCacheInsert.get(1), nullptr);
+    ASSERT_EQ(autoCacheGet.get(1), nullptr);
+    ASSERT_NE(manCache.get(1), nullptr);
+}
+
+TEST(LruTimeoutCache, Works) {
+    ag::LruTimeoutCache<size_t, std::string> cache(CACHE_SIZE, std::chrono::milliseconds(TIMEOUT_MS), true);
+    // initialize, check size and `get` func
+    for (size_t i = 0; i < CACHE_SIZE; ++i) {
+        cache.insert(i, std::to_string(i));
+        ASSERT_EQ(cache.size(), i + 1);
+        auto v = cache.get(i);
+        ASSERT_TRUE(v);
+        ASSERT_EQ(*v, std::to_string(i));
+    }
+
+    // remove every second entry
+    for (size_t i = 0; i < CACHE_SIZE; i += 2) {
+        cache.erase(i);
+    }
+
+    // check that size corresponds to the entries number
+    ASSERT_EQ(cache.size(), CACHE_SIZE / 2);
+
+    // check that removed entries were deleted and other ones are still in cache
+    for (size_t i = 0; i < CACHE_SIZE; ++i) {
+        if ((i % 2) == 0) {
+            ASSERT_FALSE(cache.get(i));
+        } else {
+            ASSERT_TRUE(cache.get(i));
+        }
+    }
+}
+
+TEST(LruTimeoutCache, DoesNotLeak) {
+    using namespace std::chrono_literals;
+    ag::LruTimeoutCache<int, std::string> cache(3, 1h, true);
+
+    cache.insert(1, "a");
+    cache.insert(2, "b");
+    ag::SteadyClock::add_time_shift(90min);
+    cache.insert(3, "c");
+
+    // Check that timed out entries were deleted from everywhere
+    ASSERT_EQ(1, cache.size());
+    ASSERT_EQ(cache.size(), cache.m_timeout_keys.size());
+    ASSERT_EQ(cache.m_timeout_keys.size(), cache.m_keys_timeout_iters.size());
+
+    cache.insert(4, "d");
+    cache.insert(5, "e");
+    cache.insert(6, "f");
+    cache.insert(7, "g");
+    cache.insert(8, "h");
+
+    // Check that evicted entries were deleted from everywhere
+    ASSERT_EQ(3, cache.size());
+    ASSERT_EQ(cache.size(), cache.m_timeout_keys.size());
+    ASSERT_EQ(cache.m_timeout_keys.size(), cache.m_keys_timeout_iters.size());
 }
