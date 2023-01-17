3006c5337737aa6d4fc31f04917f4a64330dfbff
Sergei Gunchenko <s.gunchenko@adguard.com>
2023-01-17 14:37:17 +0300

    Pull request 83: Allow exporting a single version to local conan cache
    Merge in ADGUARD-CORE-LIBS/native-libs-common from fix/export to master
    
    Squashed commit of the following:
    
    commit 2c6b83f0e3bc05004f0e85b1e9c0d3d44426b4cf
    Author: Sergei Gunchenko <s.gunchenko@adguard.com>
    Date:   Tue Jan 17 14:20:53 2023 +0300
    
        do not try to restore original commit
    
    commit cfdb16245bfd951cf6dfd0d4083bfcbb255ceb6e
    Author: Sergei Gunchenko <s.gunchenko@adguard.com>
    Date:   Tue Jan 17 13:40:32 2023 +0300
    
        improve script
    
    commit 43759645dc46f95bbbab07f849fa77e130d52191
    Author: Sergei Gunchenko <s.gunchenko@adguard.com>
    Date:   Tue Jan 17 10:36:43 2023 +0300
    
        do not check conan export
    
    commit 4a603e98ef7561da25cc76c3f2fbf34590e5bd0f
    Author: Sergei Gunchenko <s.gunchenko@adguard.com>
    Date:   Mon Jan 16 21:12:48 2023 +0300
    
        more reliable check
    
    commit 42bee16d32c93ba518433392550de940b22f63a8
    Author: Sergei Gunchenko <s.gunchenko@adguard.com>
    Date:   Mon Jan 16 19:22:57 2023 +0300
    
        Allow exporting a single version to local conan cache

--- scripts/export_conan.py
+++ scripts/export_conan.py
@@ -1,33 +1,57 @@
 #!/usr/bin/env python3
 
+"""
+This script exports the NativeLibsCommon (NLC) package and all the packages
+from `conan/recipes` to the local conan cache.
+
+By default, it exports the last version from `conandata.yml` of the NLC and
+the special `777` version (i.e., current master in case a commit hash is not
+specified).
+
+Pass `all` as an argument to export all versions (i.e., `export_conan.py all`).
+
+Pass a version number as an argument to export only the specific version
+(e.g., `export_conan.py 1.0.0`).
+"""
+
 import os
 import subprocess
+import sys
+
 import yaml
 
 work_dir = os.path.dirname(os.path.realpath(__file__))
 project_dir = os.path.dirname(work_dir)
 recipes_dir = os.path.join(project_dir, 'conan', 'recipes')
-global yaml_data
+commit_hash_version = "777"
 
-versions = ["777"]
-conandata_path = os.path.join(project_dir, "conandata.yml")
-if os.path.exists(conandata_path):
-    yaml_data = yaml.safe_load(open(conandata_path, "r"))
+with open(os.path.join(project_dir, "conandata.yml"), "r") as file:
+    yaml_data = yaml.safe_load(file)
+
+versions = []
+if len(sys.argv) == 1:
+    versions.append(commit_hash_version)
+    if len(yaml_data["commit_hash"]) > 0:
+        versions.append(list(yaml_data["commit_hash"].keys())[-1])
+elif sys.argv[1] == "all":
+    versions.append(commit_hash_version)
     for version in yaml_data["commit_hash"]:
-         versions.append(version)
-
-for folder in os.listdir(recipes_dir):
-    path = os.path.join(recipes_dir, folder)
-    if os.path.isdir(path):
-        subprocess.run(["conan", "export", path, "AdguardTeam/NativeLibsCommon"])
+        versions.append(version)
+else:
+    versions.append(sys.argv[1])
 
 for version in versions:
-    if (version == "777"):
-        subprocess.run(["git", "checkout", "master"])
+    if version == commit_hash_version:
+        subprocess.run(["git", "checkout", "master"], check=True)
     else:
         hash1 = yaml_data["commit_hash"][version]["hash"]
-        result = subprocess.run(["git", "log", "--reverse", "--ancestry-path", hash1 + "..master", "--pretty=%h"], capture_output=True)
+        result = subprocess.run(["git", "log", "--reverse", "--ancestry-path", hash1 + "..master", "--pretty=%h"],
+                                check=True, capture_output=True)
         the_hash = result.stdout.decode().splitlines()[0]
         print("HASH is ", the_hash)
-        subprocess.run(["git", "checkout", the_hash])
+        subprocess.run(["git", "checkout", the_hash], check=True)
     subprocess.run(["conan", "export", project_dir, "/" + version + "@AdguardTeam/NativeLibsCommon"])
+    for folder in os.listdir(recipes_dir):
+        path = os.path.join(recipes_dir, folder)
+        if os.path.isdir(path):
+            subprocess.run(["conan", "export", path, "AdguardTeam/NativeLibsCommon"], check=True)
