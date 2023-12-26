4a4e07b6e80efdfbd32106f6868ab6663b60417d
Sergey Fionov <sfionov@adguard.com>
2023-12-26 20:25:35 +0200

    Fix libuv

--- conan2/recipes/libuv/conanfile.py
+++ conan2/recipes/libuv/conanfile.py
@@ -68,6 +68,7 @@ class libuvConan(ConanFile):
         cmake.install()
 
     def package_info(self):
+        self.cpp_info.set_property("cmake_file_name", "libuv")
         self.cpp_info.set_property("cmake_target_name", "uv" if self.options.shared else "uv_a")
         self.cpp_info.libs = ["uv" if self.options.shared else "uv_a"]
         self.cpp_info.set_property("pkg_config_name", "libuv" if self.options.shared else "libuv-static")
