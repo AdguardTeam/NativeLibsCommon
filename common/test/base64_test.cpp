075c21ed1bab21c0899a5347004fa361d5e78bf5
Andrey Yakushin <a.yakushin@adguard.com>
2024-10-17 12:23:48 +0400

    Port base64 tests from core

---
+++ common/test/base64_test.cpp
@@ -0,0 +1,41 @@
+#include <gtest/gtest.h>
+
+#include "common/base64.h"
+
+#include <string>
+#include <string_view>
+
+static std::string to_str(const std::optional<std::vector<uint8_t>> &vec) {
+    if (!vec.has_value()) {
+        return "";
+    }
+    return  {vec->begin(), vec->end()};
+}
+
+TEST(base64, basic) {
+    std::string_view encoded = "SGVsbG8sIHdvcmxkIQ==";
+    std::string_view expect = "Hello, world!";
+    auto decoded = ag::decode_base64(encoded, false);
+    ASSERT_TRUE(decoded.has_value());
+    ASSERT_EQ(expect, to_str(decoded));
+    ASSERT_EQ(ag::encode_to_base64({ (uint8_t *)decoded->data(), decoded->size() }, false), encoded);
+    ASSERT_FALSE(ag::decode_base64(encoded.substr(0, 5), false).has_value());
+}
+
+TEST(base64, decode) {
+    ASSERT_EQ("\xfa", to_str(ag::decode_base64("+g==", false)));
+    ASSERT_EQ("\xfe", to_str(ag::decode_base64("/g==", false)));
+    ASSERT_EQ("\xfa", to_str(ag::decode_base64("+g=", false)));
+    ASSERT_EQ("\xfe", to_str(ag::decode_base64("/g=", false)));
+    ASSERT_EQ("\xfa", to_str(ag::decode_base64("+g", false)));
+    ASSERT_EQ("\xfe", to_str(ag::decode_base64("/g", false)));
+}
+
+TEST(base64, decodeurl) {
+    ASSERT_EQ(std::vector<uint8_t>({0xfa}), ag::decode_base64("-g==", true));
+    ASSERT_EQ(std::vector<uint8_t>({0xfe}), ag::decode_base64("_g==", true));
+    ASSERT_EQ(std::vector<uint8_t>({0xfa}), ag::decode_base64("-g=", true));
+    ASSERT_EQ(std::vector<uint8_t>({0xfe}), ag::decode_base64("_g=", true));
+    ASSERT_EQ(std::vector<uint8_t>({0xfa}), ag::decode_base64("-g", true));
+    ASSERT_EQ(std::vector<uint8_t>({0xfe}), ag::decode_base64("_g", true));
+}
