6d46c6c7fd270bb0a92b2b76d8afacce463d214f
Andrey Yakushin <a.yakushin@adguard.com>
2025-10-16 21:11:05 +0300

    Pull request 204: Extend AutoFd functional
    Squashed commit of the following:
    
    commit 2e07d95c498eb42d22a8aba30731280af8afee45
    Merge: cfa5d7c a83d539
    Author: Andrey Yakushin <a.yakushin@adguard.com>
    Date:   Thu Oct 16 21:58:15 2025 +0400
    
        Merge remote-tracking branch 'origin/master' into fix/AG-45340
    
    commit cfa5d7c8e4fab7a4d2c5135d94e127aece399b85
    Author: Andrey Yakushin <a.yakushin@adguard.com>
    Date:   Mon Oct 13 15:28:01 2025 +0400
    
        Remove static_assert
    
    commit 4e598fc788d4d2ec9e0cadaf67f1ac47e4a2b7fb
    Author: Andrey Yakushin <a.yakushin@adguard.com>
    Date:   Mon Oct 13 12:31:01 2025 +0400
    
        Better cross platform for autofd
    
    commit b67dda6365dd4d06bc0277ab302ea3dfefc45989
    Author: Andrey Yakushin <a.yakushin@adguard.com>
    Date:   Fri Oct 3 14:32:38 2025 +0400
    
        Remove misleading docs
    
    commit c70c2bfe0503f22dc586bfa0ab2a4cf10f95f98b
    Author: Andrey Yakushin <a.yakushin@adguard.com>
    Date:   Fri Oct 3 14:24:04 2025 +0400
    
        Extend AutoFd functional and add docs

--- common/include/common/autofd.h
+++ common/include/common/autofd.h
@@ -3,44 +3,137 @@
 #include <event2/util.h>
 #include <utility>
 
+#ifndef _WIN32
+#include <unistd.h>
+#endif
+
 namespace ag {
 
+/**
+ * A class for automatic file descriptor management
+ *
+ * This class manages a file descriptor, ensuring it's closed when the object is destroyed.
+ * It supports both duplication and moving of file descriptors.
+ *
+ * @note This class can't be used to manage file descriptors obtained with `_open`, `_wopen`,
+ *       `_sopen_s` and `_wsopen_s` methods on Windows.
+ */
 class AutoFd {
-    evutil_socket_t m_fd = -1;
+    evutil_socket_t m_fd = EVUTIL_INVALID_SOCKET;
 public:
+    /**
+     * Default constructor
+     */
     AutoFd() = default;
-    explicit AutoFd(evutil_socket_t fd) : m_fd(fd) {}
+
+#ifndef _WIN32
+    /**
+     * Create an AutoFd by duplicating a file descriptor
+     *
+     * @param fd File descriptor to duplicate
+     * @return AutoFd object managing the duplicated file descriptor
+     */
+    static AutoFd dup_fd(evutil_socket_t fd) {
+        AutoFd result;
+        if (fd != EVUTIL_INVALID_SOCKET) {
+            result.m_fd = ::dup(fd);
+        }
+        return result;
+    }
+#endif
+
+    /**
+     * Create an AutoFd by adopting a file descriptor
+     *
+     * @param fd File descriptor to adopt (take ownership of)
+     * @return AutoFd object managing the adopted file descriptor
+     */
+    static AutoFd adopt_fd(evutil_socket_t fd) {
+        AutoFd result;
+        if (fd != EVUTIL_INVALID_SOCKET) {
+            result.m_fd = fd;
+        }
+        return result;
+    }
+
+    /**
+     * Destructor
+     *
+     * Closes the file descriptor if it's valid
+     */
     ~AutoFd() {
         reset();
     }
 
-    evutil_socket_t get() const {
+    /**
+     * Get the file descriptor
+     *
+     * @return The file descriptor
+     */
+    [[nodiscard]] evutil_socket_t get() const {
         return m_fd;
     }
 
-    evutil_socket_t release() {
-        evutil_socket_t fd = std::exchange(m_fd, -1);
+    /**
+     * Check if the file descriptor is valid
+     *
+     * @return true if the file descriptor is valid, false otherwise
+     */
+    [[nodiscard]] bool is_valid() const {
+        return m_fd != EVUTIL_INVALID_SOCKET;
+    }
+
+    /**
+     * Release the file descriptor
+     *
+     * @return The file descriptor, which is no longer managed by this object
+     */
+    [[nodiscard]] evutil_socket_t release() {
+        evutil_socket_t fd = std::exchange(m_fd, EVUTIL_INVALID_SOCKET);
         return fd;
     }
 
-    void reset(evutil_socket_t new_fd = -1) {
+    /**
+     * Reset the file descriptor and replace it with new one
+     *
+     * @param new_fd File descriptor to replace with
+     */
+    void reset(evutil_socket_t new_fd = EVUTIL_INVALID_SOCKET) {
         evutil_socket_t fd = std::exchange(m_fd, new_fd);
-        if (fd != -1 && new_fd != fd) {
+        if (fd != EVUTIL_INVALID_SOCKET && new_fd != fd) {
             evutil_closesocket(fd);
         }
     }
 
+    /**
+     * Move constructor
+     *
+     * @param other AutoFd to move from
+     */
     AutoFd(AutoFd &&other) noexcept {
         *this = std::move(other);
     }
 
+    /**
+     * Move assignment operator
+     *
+     * @param other AutoFd to move from
+     * @return Reference to this object
+     */
     AutoFd &operator=(AutoFd &&other) noexcept {
         reset();
-        m_fd = std::exchange(other.m_fd, -1);
+        m_fd = std::exchange(other.m_fd, EVUTIL_INVALID_SOCKET);
         return *this;
     }
 
+    /**
+     * Copy constructor (deleted)
+     */
     AutoFd(const AutoFd &) = delete;
+
+    /**
+     * Copy assignment operator (deleted)
+     */
     void operator=(const AutoFd &) = delete;
 };
 
