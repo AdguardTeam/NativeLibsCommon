b63779c67e29f927d160261436e00478cce00b9d
Ilia Zhirov <i.zhirov@adguard.com>
2026-05-27 23:26:55 +0500

    Add more information to get_cert_diagnostic_info

--- tls/include/common/tls/cert_utils.h
+++ tls/include/common/tls/cert_utils.h
@@ -7,7 +7,9 @@
 namespace ag::tls {
 
 /**
- * Format certificate diagnostic info (Subject, Issuer, Chain length).
+ * Format detailed certificate diagnostic info.
+ * For the leaf cert and each cert in the chain, outputs:
+ * Subject, Issuer, SHA256 fingerprint, NotBefore, NotAfter.
  * Returns an empty string if cert is null.
  */
 std::string get_cert_diagnostic_info(X509 *cert, STACK_OF(X509) *chain);
